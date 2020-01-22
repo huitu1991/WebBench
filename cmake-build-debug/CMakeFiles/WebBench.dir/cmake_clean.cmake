aea4317ece87f09cbba4dad6e9b6bbc8a127b3cf
huitu1991 <huitu1991@gmail.com>
2020-01-22 08:41:50 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/WebBench.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebBench.dir/webbench.c.o"
+  "WebBench.pdb"
+  "WebBench"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WebBench.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
